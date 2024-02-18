ec9a7ed5e5399b0adac2f541fdb2dafe5ee5996f
SeaSkyThe <marceloer2011@gmail.com>
2024-02-17 23:49:26 -0300

    feat: new exercises solved

--- bin/main.ml
+++ bin/main.ml
@@ -1,7 +1,10 @@
-(* let () = Printf.printf "%s\n" BabyCaml.En.v;; *)
-(* let () = Printf.printf "%s\n" BabyCaml.Es.v;; *)
 
-
+(* HELPERS *)
+let print_tuple_list lst =
+  let print_element (x, y) =
+    Printf.printf "(%d, %s) " x y in
+  List.iter print_element lst;
+  Printf.printf "\n"
 
 (* EXERCISES - LISTS *)
 let () = Printf.printf "THOSE ARE MY LIST EXERCISES RUNNING!!! IM A GROWING CAMEL\n\n"
@@ -18,8 +21,22 @@ let () = Printf.printf "\n\n\n"
 let () = Printf.printf "2 - Tail of a list: \n"
 let () = let list = ["a"; "b"; "c"; "d"; "e"] in
 match BabyCaml.Lists_exercises.tail list with
-| Some result -> Printf.printf "%s\n" result
-| None -> Printf.printf "Lista vazia \n"
+| Some result -> Printf.printf "%s\n\n" result
+| None -> Printf.printf "Lista vazia \n\n"
 
+(* Run-Length Encoding *)
+let original_list = ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"];;
+let () = Printf.printf "3 - Run-Length Enconding: "
+let () = List.iter (Printf.printf "%s ") original_list
+let () = Printf.printf "\nResult: "
+let encoded_list = BabyCaml.Lists_exercises.rle original_list
+let () = print_tuple_list encoded_list
 
-
+(* Remove the K'th Element From a List *)
+let original_list = ["a"; "b"; "c"; "d"]
+let () = Printf.printf "\n\n\n4 - Remove the K'th Element from a List: "
+let () = List.iter (Printf.printf "%s ") original_list
+let () = Printf.printf "\nResult: "
+let result_list = BabyCaml.Lists_exercises.remove_kth 1 original_list
+let () = List.iter (Printf.printf "%s ") result_list
+let () = Printf.printf "\n\n\n"
--- lib/lists_exercises.ml
+++ lib/lists_exercises.ml
@@ -1,14 +1,32 @@
 (* REVERSE A LIST *)
+(* https://ocaml.org/exercises?difficulty_level=beginner#5 *)
 let rec rev list acc = match list with
-| [] -> acc 
+| [] -> acc
 | h :: t -> rev t (h :: acc);;
 
 
 (* TAIL OF A LIST *)
+(* https://ocaml.org/exercises?difficulty_level=beginner#1 *)
 let rec tail list = match list with
 | [] -> None
 | [x] -> Some x
 | _ :: t -> tail t;;
+
+(* Run-Length Encoding *)
+(* https://ocaml.org/exercises?difficulty_level=beginner#10 *)
+let rle list =
+    let rec aux count acc output = match output with
+        | [] -> []
+        | [x] -> (count+1, x) :: acc
+        | a :: (b :: _ as t) -> if a = b then aux (count + 1) acc t
+                                else aux 0 ((count + 1, a) :: acc) t in
+    rev (aux 0 [] list) [];;
+
+(* Remove the K'th Element From a List *)
+(* https://ocaml.org/exercises?difficulty_level=beginner#20 *)
+let rec remove_kth k list = match list with
+    | [] -> []
+    | h :: t -> if k = 0 then t else h :: remove_kth (k - 1) t
 
 
 
